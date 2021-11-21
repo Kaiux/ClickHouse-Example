6136199a32292fd7900791039c3464cd0bb7c33c
Kaiux <kaiux@foxmail.com>
2021-11-21 19:07:09 +0800

    init spring boot application

---
+++ src/main/java/cn/kaiux/clickhouse/example/ClickHouseExampleApplication.java
@@ -0,0 +1,17 @@
+package cn.kaiux.clickhouse.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : likai.yu
+ * @date : 2021/11/21
+ */
+@SpringBootApplication
+public class ClickHouseExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickHouseExampleApplication.class, args);
+    }
+
+}
